9738d27a723e3135cba8713c4b267bc04dee80ba
lamine9819 <lamineb29@gmail.com>
2025-07-09 09:14:51 +0000

    premier commit

---
+++ cra-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // ← assure-toi que ceci est bien présent
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
